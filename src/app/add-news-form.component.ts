8900a5f086f548e418b664fff532b68f9f16c13b
Andre Shulga <solyariz@gmail.com>
2017-05-02 17:58:25 +0600

    added form

--- src/app/add-news-form.component.ts
+++ src/app/add-news-form.component.ts
@@ -2,7 +2,63 @@ import {Component} from '@angular/core';
 
 @Component({
     template: `
-        <h2>Add news</h2>
+        <h2>Add news</h2><br/>
+        <table border="0">
+
+            <div style="padding:1px">
+
+                <tr>
+                    <td>
+                        <div style="float:left;padding-right:8px;">
+                            Title
+                        </div>
+                    </td>
+                    <td>
+                        <input/>
+                    </td>
+                </tr>
+            </div>
+
+            <div style="padding:1px">
+                <tr>
+                    <td>
+                        <div style="float:left;padding-right:8px;">
+                            Date
+                        </div>
+                    </td>
+                    <td>
+                        <input/>
+                    </td>
+                </tr>
+            </div>
+
+            <div style="padding:1px">
+                <tr>
+                    <td>
+                        <div style="float:left;padding-right:8px;">
+                            Brief
+                        </div>
+                    </td>
+                    <td>
+                        <input/>
+                    </td>
+                </tr>
+            </div>
+
+            <div style="padding:1px">
+                <tr>
+                    <td>
+                        <div style="float:left;padding-right:8px;">
+                            Content
+                        </div>
+                    </td>
+                    <td>
+                        <input/>
+                    </td>
+                </tr>
+            </div>
+
+        </table>
     `
 })
 export class AddNewsFormComponent {
