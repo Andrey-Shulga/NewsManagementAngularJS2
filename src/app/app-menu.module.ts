5e1aac03c04a998dff1a7e5b81ae0408205c4507
Andre Shulga <solyariz@gmail.com>
2017-05-02 16:20:14 +0600

    menu in separate module

---
+++ src/app/app-menu.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {NewsListComponent} from './news-list.component';
+import {AddNewsFormComponent} from './add-news-form.component';
+import {PageNotFoundComponent} from './not-found.component';
+
+const appRoutes: Routes = [
+    {path: 'news-list', component: NewsListComponent},
+    {path: 'add-news', component: AddNewsFormComponent},
+    {path: '', redirectTo: '/news-list', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ],
+    providers: []
+})
+
+export class AppMenuModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,19 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
+import {AppMenuModule} from './app-menu.module';
+
 import {NewsListComponent} from './news-list.component';
 import {AddNewsFormComponent} from './add-news-form.component';
 import {PageNotFoundComponent} from './not-found.component';
 
-
-const appRoutes: Routes = [
-    {path: 'news-list', component: NewsListComponent},
-    {path: 'add-news', component: AddNewsFormComponent},
-    {path: '', redirectTo: '/news-list', pathMatch: 'full'},
-    {path: '**', component: PageNotFoundComponent}
-];
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot(appRoutes),
+        AppMenuModule
     ],
     declarations: [
         AppComponent,
