0176235a2b41819ffd3039759c4f878a3b6747d8
Andre Shulga <solyariz@gmail.com>
2017-05-04 17:47:05 +0600

    refactoring

--- src/app/news-list/news-list.component.ts
+++ src/app/news-list/news-list.component.ts
@@ -11,18 +11,18 @@ export class NewsListComponent implements OnInit {
     title = 'News list';
     newsList: News[];
 
+    constructor(private newsService: NewsService) {
+    }
+
     ngOnInit() {
+        this.getAll();
+    }
+
+    getAll() {
         this.newsService.getAll()
             .subscribe(resBody => this.newsList = resBody,
                 error => console.error('Error: ' + error),
                 () => console.log('Get news list completed!', this.newsList)
             );
-    }
-
-    goToForm() {
-
-    }
-
-    constructor(private newsService: NewsService) {
     }
 }
