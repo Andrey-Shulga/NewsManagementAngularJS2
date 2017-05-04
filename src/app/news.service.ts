0db76174975d7895965aba616c3ef7124ddac912
Andre Shulga <solyariz@gmail.com>
2017-05-04 12:14:54 +0600

    edit button

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,15 +1,15 @@
-import {Injectable} from "@angular/core";
-import {Headers, Http} from "@angular/http";
-import "rxjs/add/operator/toPromise";
-import "rxjs/add/operator/map";
+import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
 
-import {News} from "./model/news";
-import {Observable} from "rxjs/Observable";
+import {News} from './model/news';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class NewsService {
 
-    private saveNewsPostRequest = 'http://localhost:8082/rest/news/save';
+    private saveNewsPostRequest = 'http://localhost:8081/rest/news/save';
     private headers = new Headers({'Content-Type': 'application/json;charset=UTF-8'});
 
     constructor(private http: Http) {
