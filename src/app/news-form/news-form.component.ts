9eb581f7756f72607404d98d045a04b5885591a1
Andre Shulga <solyariz@gmail.com>
2017-05-04 17:45:42 +0600

    refactoring

--- src/app/news-form/news-form.component.ts
+++ src/app/news-form/news-form.component.ts
@@ -22,6 +22,10 @@ export class AddNewsFormComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.getById();
+    }
+
+    getById() {
         this.route.params.subscribe((params: Params) => {
             this.newsId = params['id']
         });
